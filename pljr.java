8ebdce300d1b3de004c4d0bc007cf0ca9951d3de
veryabd123 <95065227+veryabd123@users.noreply.github.com>
2022-04-07 09:53:38 +0700

    Add files via upload

---
+++ pljr.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pbo;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface pljr {
+    double konten();
+    double visual();
+    double design();
+    double hasil();
+}
